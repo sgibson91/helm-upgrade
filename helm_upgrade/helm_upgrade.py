3136ed96e42ac450bb164f62d6b2608e9de695c7
Sarah Gibson <sgibson@turing.ac.uk>
2020-02-10 10:46:20 +0000

    Remove GitHub functionality from helm_upgrade class

--- helm_upgrade/helm_upgrade.py
+++ helm_upgrade/helm_upgrade.py
@@ -1,4 +1,4 @@
-"""Use GitHub API to update Helm Chart dependencies."""
+"""Update Helm Chart dependencies."""
 
 
 def logging():
@@ -16,19 +16,15 @@ def logging():
 
 class HelmUpgrade:
     """
-    HelmUpgrade class for interacting with the GitHub API and making changes to
-    a Helm Chart requirements file.
+    HelmUpgrade class for interacting with the Helm Chart repos and making
+    changes to a local Helm Chart requirements file.
 
     Attributes:
-        target_repo (str): GitHub organisation/repo containing a Helm Chart
         chart (str): The Helm Chart name
         dependencies (dict): A dictionary of helm chart dependencies and their
                              repos
-        token (str): GitHub access token
-        branch (str): Name of git branch to commit to
         dry_run (Bool): Whether the changes should be pushed or not
         verbose (Bool): Whether to turn on logging or not
-        repo_api (str): the API URL for the target repo
     """
 
     def __init__(self, argsDict):
@@ -39,16 +35,6 @@ class HelmUpgrade:
         """
         for k, v in argsDict.items():
             setattr(self, k, v)
-
-        # Set the repo API
-        self.repo_api = f"https://api.github.com/repos/{argsDict['target_repo']}/"
-
-        # Remove any existing forks
-        self.remove_fork()
-
-        # Get the GitHub access token
-        if self.token is None:
-            self.get_token()
 
         # Turn on logging
         if self.verbose:
