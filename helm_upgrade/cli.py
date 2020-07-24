7a46241b34e32c346cf7bb1f614aecec0a4ae546
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-24 20:24:37 +0100

    ♻️ Reformat parser into a func

--- helm_upgrade/cli.py
+++ helm_upgrade/cli.py
@@ -6,45 +6,53 @@ from .app import helm_upgrade
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-DESCRIPTION = "Update the dependencies of a local Helm Chart in a project repository."  # noqa: E501
-parser = argparse.ArgumentParser(description=DESCRIPTION)
-subparsers = parser.add_subparsers()
 
-version_parser = subparsers.add_parser(
-    "version", help="Print the version and exit"
-)  # noqa: E501
+def parse_args(args):
+    """Parse arguments from the command line"""
+    DESCRIPTION = "Update the dependencies of a local Helm Chart in a project repository."  # noqa: E501
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    subparsers = parser.add_subparsers()
 
-run_parser = subparsers.add_parser(
-    "run", help="Update the dependencies of a helm chart"
-)
+    version_parser = subparsers.add_parser(  # noqa: F841
+        "version", help="Print the version and exit"
+    )  # noqa: E501
 
-run_parser.add_argument(
-    "chart", type=str, help="Name of the local Helm Chart to be updated."
-)
+    run_parser = subparsers.add_parser(
+        "run", help="Update the dependencies of a helm chart"
+    )
 
-run_parser.add_argument(
-    "dependencies",
-    type=json.loads,
-    help="""A dictionary of Helm Chart dependencies and their host repo URLs.
-    E.g. '{"nginx-ingress":
-    "https://raw.githubusercontent.com/helm/charts/master/stable/nginx-ingress/Chart.yaml"}'
-    """,
-)
+    run_parser.add_argument(
+        "chart", type=str, help="Name of the local Helm Chart to be updated."
+    )
 
-run_parser.add_argument(
-    "--dry-run",
-    action="store_true",
-    help="Perform a dry run of the update. Don't write the changes to a file.",
-)
+    run_parser.add_argument(
+        "dependencies",
+        type=json.loads,
+        help="""A dictionary of Helm Chart dependencies and their host repo URLs.
+        E.g. '{"nginx-ingress":
+        "https://raw.githubusercontent.com/helm/charts/master/stable/nginx-ingress/Chart.yaml"}'
+        """,
+    )
 
-run_parser.add_argument(
-    "-v", "--verbose", action="store_true", help="Option to turn on logging."
-)
+    run_parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Perform a dry run of the update. Don't write the changes to a file.",  # noqa: E501
+    )
+
+    run_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Option to turn on logging.",  # noqa: E501
+    )
+
+    return parser.parse_args()
 
 
 def main():
     """Main function"""
-    args = parser.parse_args(sys.argv[1:])
+    args = parse_args(sys.argv[1:])
 
     if not vars(args):
         about = {}
