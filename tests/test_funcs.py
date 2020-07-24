dc5905f69d382e6ed4b6e22cb9e42c1bbe008dea
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-24 20:10:51 +0100

    ✅ Add a new test

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,4 +1,5 @@
-from helm_upgrade.app import check_chart_versions
+import os
+from helm_upgrade.app import check_chart_versions, get_local_chart_versions
 
 
 def test_check_chart_versions_match():
@@ -27,3 +28,14 @@ def test_check_chart_versions_no_match():
 
     assert len(check_chart_versions(test_current_deps, test_new_deps)) == 1
     assert check_chart_versions(test_current_deps, test_new_deps) == ["cat"]
+
+
+def test_get_local_chart_versions():
+    chart_name = os.path.join("tests", "test-chart")
+    test_deps = {
+        "binderhub": "0.2.0-n079.h351d336",
+        "nginx-ingress": "1.29.5",
+        "cert-manager": "v0.10.0",
+    }
+
+    assert get_local_chart_versions(chart_name) == test_deps
