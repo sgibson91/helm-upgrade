ab7f2b3cbf2e59e8034fe6abfa6769717e4abec1
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-24 22:25:04 +0100

    ✅ Add a new test

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -3,6 +3,7 @@ import logging
 from testfixtures import log_capture
 from helm_upgrade.app import check_chart_versions, get_local_chart_versions
 
+HERE = os.getcwd()
 
 def test_check_chart_versions_match():
     test_current_deps = {
@@ -102,3 +103,23 @@ def test_get_local_chart_versions_broken():
     }
 
     assert get_local_chart_versions(chart_name) != test_deps
+
+
+@log_capture()
+def test_get_local_chart_versions_verbose(capture):
+    chart_name = os.path.join("tests", "test-chart")
+    filepath = os.path.join(HERE, chart_name, "requirements.yaml")
+
+    logger = logging.getLogger()
+    logger.info("Reading local chart dependencies from: %s" % filepath)
+
+    test_deps = {
+        "binderhub": "0.2.0-n079.h351d336",
+        "nginx-ingress": "1.29.5",
+        "cert-manager": "v0.10.0",
+    }
+
+    result = get_local_chart_versions(chart_name, verbose=True)
+
+    assert result == test_deps
+    capture.check_present()
